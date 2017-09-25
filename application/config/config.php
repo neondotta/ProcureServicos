24a795c0d46c35c1aa7632f94088b52dabc9dea4
Neon Dotta <neondotta@gmail.com>
2017-09-25 08:53:14 -0300

    config heroku

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/procure-servicos2';
+$config['base_url'] = 'http://localhost/procure-servicos';
+//$config['base_url'] = 'https://procureservicos.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
