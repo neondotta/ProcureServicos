4adeb5bcdc804f97b8b040dc27f0586b06ee3309
Neon Dotta <neondotta@gmail.com>
2017-09-26 08:56:40 -0300

    htaccess, sistema de login e mudanças no layout

---
+++ application/models/user/UserModel.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Neon Dotta
+ * Date: 25/09/2017
+ * Time: 23:18
+ */
+
+class UserModel extends CI_Model
+{
+
+    public function insertUser($user)
+    {
+        $this->db->insert("user", $user);
+    }
+
+    public function authLogin($email, $password)
+    {
+        $filter = ['email' => $email, 'password' => $password];
+        $this->db->where($filter);
+        $user = $this->db->get('user')->row_array();
+        return $user;
+    }
+
+}
