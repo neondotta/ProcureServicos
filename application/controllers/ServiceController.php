6f4025c565eb25c77d419c59c73926b1652b5f32
Neon Dotta <neondotta@gmail.com>
2017-12-20 22:27:05 -0200

    Ajuste no modal de avaliação

--- application/controllers/ServiceController.php
+++ application/controllers/ServiceController.php
@@ -205,7 +205,8 @@ class ServiceController extends IndexCore
             "description" => $description
         );
 
-        return $this->ServiceModel->serviceRating($params);
+        $this->ServiceModel->serviceRating($params);
 
+        echo 'FEITOOO';
     }
 }   
